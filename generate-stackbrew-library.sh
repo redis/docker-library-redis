f7e5af50379ba98937019e0a9dddab176c9b61dc
Joshua Delsman <joshua.delsman@hotelstonight.com>
2015-04-01 10:26:24 -0700

    adding 3.0

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,7 +3,8 @@ set -e
 
 declare -A aliases
 aliases=(
-	[2.8]='2 latest'
+	[2.8]='2'
+	[3.0]='3 latest'
 )
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
@@ -17,14 +18,14 @@ echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 for version in "${versions[@]}"; do
 	commit="$(git log -1 --format='format:%H' -- "$version")"
 	fullVersion="$(grep -m1 'ENV REDIS_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
-	
+
 	versionAliases=()
 	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
 		versionAliases+=( $fullVersion )
 		fullVersion="${fullVersion%[.-]*}"
 	done
 	versionAliases+=( $version ${aliases[$version]} )
-	
+
 	echo
 	for va in "${versionAliases[@]}"; do
 		echo "$va: ${url}@${commit} $version"
