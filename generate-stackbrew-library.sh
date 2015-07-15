5e3f74f3edbbf8311b86e40e7ebe47f602981387
Tianon Gravi <admwiggin@gmail.com>
2015-07-15 13:59:02 -0700

    Add 32bit variants
    See also http://redis.io/topics/memory-optimization#using-32-bit-instances

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -30,4 +30,18 @@ for version in "${versions[@]}"; do
 	for va in "${versionAliases[@]}"; do
 		echo "$va: ${url}@${commit} $version"
 	done
+	
+	for variant in 32bit; do
+		[ -f "$version/$variant/Dockerfile" ] || continue
+		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+		echo
+		for va in "${versionAliases[@]}"; do
+			if [ "$va" = 'latest' ]; then
+				va="$variant"
+			else
+				va="$va-$variant"
+			fi
+			echo "$va: ${url}@${commit} $version/$variant"
+		done
+	done
 done
