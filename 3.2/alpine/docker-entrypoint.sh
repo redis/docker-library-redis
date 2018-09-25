53348c1c52c3d2e8666fbf748bc9e2297c35b452
Joe Ferguson <joe@infosiftr.com>
2018-09-25 12:57:10 -0700

    Only chown things in the entrypoint that are not already owned by redis

--- 3.2/alpine/docker-entrypoint.sh
+++ 3.2/alpine/docker-entrypoint.sh
@@ -9,7 +9,7 @@ fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	chown -R redis .
+	find . \! -user redis -exec chown redis '{}' +
 	exec su-exec redis "$0" "$@"
 fi
 
