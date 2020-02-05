1bfaac18ee956db3456738ef7750e82615bbc2fb
Julien Godin <julien.godin@camptocamp.com>
2020-02-05 21:36:46 +0100

    Allow a new environment variable to change the UID of the docker

--- 4.0/docker-entrypoint.sh
+++ 4.0/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-
+if [ ! -z ${FORCE_DAEMON_UID+x} ];then
+	usermod -u $FORCE_DAEMON_UID redis
+	find / -user 999 -exec chown -h redis {} 2>/dev/null \; && true
+fi
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
