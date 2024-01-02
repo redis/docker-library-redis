cd5f8ce87956dc2b3e91a3a80b3b67652ed6adbe
Omer Shadmi <omer.shadmi@redis.com>
2024-01-02 03:21:50 +0200

    fixes

--- 7.2/system-setup.py
+++ 7.2/system-setup.py
@@ -42,6 +42,9 @@ class RedisSetup(paella.Setup):
     def alpine(self):
         self.install("gcc make openssl openssl-dev libatomic dev86 musl-dev")
 
+    def mariner(self):
+        self.install("gcc tar make")
+
     def common_last(self):
         if self.dist != "alpine":
             self.install("gnupg patch pkg-config") # dirmngr
